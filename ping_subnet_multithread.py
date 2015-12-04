b832fa21314879b3b413b96251aa296a8e2c437d
james <jamesmlucas@gmail.com>
2015-12-04 12:51:09 -0600

    now better allows for cli use or use as module

--- ping_subnet_multithread.py
+++ ping_subnet_multithread.py
@@ -6,70 +6,64 @@ import os
 import threading, Queue
 import sys
 import time
-import argparse
-
-parser = argparse.ArgumentParser(description='Multi-threaded host ping')
-parser.add_argument("file_list", nargs="+")
-args = parser.parse_args()
 
 safeprint = threading.Lock()
 dataQueue = Queue.Queue()
 
-# used_file = '/tmp/used_ips'
-# unused_file = '/tmp/unused_ips'
+def ping_ips(dataqueue, verbose=False):
+	""" thread function """
 
-def ping_ips(dataqueue):
-        while not dataQueue.empty():
-                ip = dataqueue.get()
-                command = ['/bin/ping', '-c', '1', '%s' % ip]
-                devnull = open(os.devnull, 'w')
-                # print('from queue: %s' % ip)
-                try:
-                        subprocess.check_call(command, stdout=devnull, stderr=devnull)
-                        with safeprint:
-                                print('In use: %s' % ip)
-                                # with open(used_file, 'a') as f:
-                                #         f.write('%s\n' % ip)
-                except subprocess.CalledProcessError as e:
-                        with safeprint:
-                                print('Unused: %s' % ip)
-                                # with open(unused_file, 'a') as f:
-                                #         f.write('%s\n' % ip)
-                except Exception as e:
-                        print('Unknown error: %s: %s' % (ip,e))
+	while not dataQueue.empty():
+		ip = dataqueue.get()
+		command = ['/bin/ping', '-c', '1', '%s' % ip]
+		devnull = open(os.devnull, 'w')
+		if verbose:
+			with safeprint:
+				print('from queue: %s' % ip)
+		try:
+			subprocess.check_call(command, stdout=devnull, stderr=devnull)
+			with safeprint:
+				print('In use: %s' % ip)
+		except subprocess.CalledProcessError as e:
+			with safeprint:
+				print('Unused: %s' % ip)
+		except Exception as e:
+			print('Unknown error: %s: %s' % (ip,e))
 
+def initialize(raw_host_list, verbose=False, numthreads=10):
+	""" parse raw_host_list and populate queues -- raw_host_list 
+		can be comprised of subnets (if so,	first break them out 
+		into indivdual IPs) or host names """
 
-#network = IPNetwork('10.0.8.1/27')
+	host_list = []
+	for item in raw_host_list:
+		# first, check if it can be turned into netaddr.IPNetwork object:
+		try:
+			host_list.extend([i for i in netaddr.IPNetwork(item)])
+		# presumably, a hostname was passed rather than an IP / IP block:
+		except netaddr.core.AddrFormatError:
+			host_list.append(item)
+		# unknown exception:
+		except Exception as e:
+			print('Unknown exception: %s' % e)
 
-# this array holds IPs for individual IPs or subnets
-# that may be passed on command line:
-network_list = []
-host_list = []
+	# populate queue with above:
+	[ dataQueue.put(host) for host in host_list ]
 
-for item in args.file_list:
-        # first, check if it can be turned into netaddr.IPNetwork object:
-        try:
-                network_list.extend([i for i in netaddr.IPNetwork(item)])
-        # presumably, a hostname was passed rather than an IP / IP block:
-        except netaddr.core.AddrFormatError:
-                host_list.append(item)
-        # unknown exception:
-        except Exception as e:
-                print('Unknown exception: %s' % e)
+	threads = []
 
-# populate queue with above:
-for ip in network_list:
-        dataQueue.put(ip)
-for host in host_list:
-        dataQueue.put(host)
+	for i in range(numthreads):
+		thread = threading.Thread(target=ping_ips, args=(dataQueue, verbose))
+		threads.append(thread)
+		# thread.daemon = True
+		thread.start()
 
-numthreads=10
-threads = []
+	for thread in threads: thread.join()
 
-for i in range(numthreads):
-        thread = threading.Thread(target=ping_ips, args=(dataQueue, ))
-	threads.append(thread)
-        # thread.daemon = True
-        thread.start()
-
-for thread in threads: thread.join()
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser(description='Multi-threaded host ping')
+	parser.add_argument("-v", "--verbose", action="store_true", default=False, help="verbose")
+	parser.add_argument("host_list", nargs="+", help="specify IPs, subnet (in cidr), or hostnames")
+	args = parser.parse_args()
+	initialize(args.host_list, args.verbose)
